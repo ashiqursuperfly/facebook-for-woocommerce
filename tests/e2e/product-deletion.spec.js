fd0cc5b9acc4c52ea4066a197b0310b467481801
Ashiqur R <ash4r@meta.com>
2025-12-06 01:29:32 +0000

    fix flake in delete test

--- tests/e2e/product-deletion.spec.js
+++ tests/e2e/product-deletion.spec.js
@@ -156,7 +156,7 @@ test.describe('Facebook for WooCommerce - Product Deletion E2E Tests', () => {
 
       // Click on Product Data Sync "Sync now" button
       console.log('🔄 Looking for Product Data Sync "Sync now" button...');
-      const syncNowButton = page.locator('#woocommerce-facebook-settings-sync-products');
+      const syncNowButton = page.locator('#woocommerce-facebook-settings-sync-products').first();
 
       if (await syncNowButton.isVisible({ timeout: 10000 })) {
         await syncNowButton.click();
